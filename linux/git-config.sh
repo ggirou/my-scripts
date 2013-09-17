635b6432c9977c56604d90a4cb45cdedcf2cc96b
Guillaume Girou <porude-git@yahoo.fr>
2013-09-17 18:45:42 +0200

    Update git-config.sh

--- linux/git-config.sh
+++ linux/git-config.sh
@@ -1,7 +1,9 @@
-defaultName="Guillaume Girou"
-read -p "user.name [$defaultName]: " username
-username=${username:-$defaultName}
-read -p "user.email: " useremail
+usernameBak=`git config --global user.name`
+useremailBak=`git config --global user.email`
+read -p "user.name [$usernameBak]: " username
+read -p "user.email [$useremailBak]: " useremail
+username=${username:-$usernameBak}
+useremail=${useremail:-$useremailBak}
 
 git config --global user.name "$username"
 git config --global user.email "$useremail"
@@ -9,5 +11,7 @@ git config --global user.email "$useremail"
 git config --global credential.helper cache
 # Set the cache to timeout after 1 hour (setting is in seconds)
 git config --global credential.helper 'cache --timeout=3600'
+git config --global push.default simple
+echo -------------------------------------------------------------------------------
 git config --list
 
