085e9b622052a0be4d71530e638fcbef19c9c7ed
Guillaume Girou <porude-git@yahoo.fr>
2014-11-04 12:13:30 +0100

    Add proxy conf with dconf (TODO)

--- linux/config/proxy-config.sh
+++ linux/config/proxy-config.sh
@@ -5,6 +5,7 @@ source ${BASH_SOURCE%/*}/../utils/commons.sh
 askForNewValue "$HTTP_PROXY" "HTTP_PROXY" HTTP_PROXY
 askForNewValue "$NO_PROXY" "NO_PROXY" NO_PROXY
 
+
 TMP=`cat <<EOF
 HTTP_PROXY=$HTTP_PROXY
 HTTPS_PROXY=$HTTP_PROXY
@@ -29,6 +30,18 @@ EOF`
 echo 'Save in /etc/apt/apt.conf.d/95proxies...'
 sudo sed -i '/::proxy/Id' /etc/apt/apt.conf.d/95proxies
 sudo sh -c "echo '$TMP' >> /etc/apt/apt.conf.d/95proxies"
+
+# TODO
+# echo 'Save in dconf...'
+# dconf write /system/proxy/mode '"manual"'
+# dconf write /system/proxy/http/enable true
+# dconf write /system/proxy/http/host "''"
+# dconf write /system/proxy/http/port ""
+# dconf write /system/proxy/http/use-authentication true
+# dconf write /system/proxy/http/authentication-user "''"
+# dconf write /system/proxy/http/authentication-password "''"
+# dconf write /system/proxy/https/host "''"
+# dconf write /system/proxy/https/port ""
 
 if dpkg -s git &> /dev/null; then
 	echo 'Configure git'
