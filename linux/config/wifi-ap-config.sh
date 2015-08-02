12b5dd4c1fc62351e8390a749f42c1a9d4f9c4a9
Guillaume Girou <porude-git@yahoo.fr>
2015-08-02 09:34:12 +0200

    Wifi scripts fixes

--- linux/config/wifi-ap-config.sh
+++ linux/config/wifi-ap-config.sh
@@ -11,38 +11,34 @@ echo
 read -e -p "Access point on: " -i "wlan0" interface
 read -e -p "Bridge on: " -i "eth0" bridge
 read -e -p "Router static IP: " -i "192.168.1.254" router_ip
-read -e -p "Access point SSID: " ssid
-read -s -e -p "Access point password: " password
 echo
 
 function configure_hostapd {
+  read -e -p "Access point SSID: " ssid
+  read -s -e -p "Access point password: " password
+
   wpa_psk=$(wpa_passphrase "$ssid" "$password") && wpa_psk=$(echo $wpa_psk | sed -r 's/.* psk=(.+) .*/\1/') || { echo "$wpa_psk"; exit 1; }
   sed -r "s/^interface=.*/interface=$interface/" -i /etc/hostapd/hostapd.conf
   sed -r "s/^ssid=.*/ssid=$ssid/" -i /etc/hostapd/hostapd.conf
   sed -r "s/^wpa_(passphrase|psk)=.*/wpa_psk=$wpa_psk/" -i /etc/hostapd/hostapd.conf
 
   sed -r 's|^#?DAEMON_CONF=.*|DAEMON_CONF="/etc/hostapd/hostapd.conf"|' -i /etc/default/hostapd
-
-  service hostapd restart
-  # hostapd /etc/hostapd/hostapd.conf
 }
 
 function configure_dhcp {
   sed -r 's/^#DHCPD/DHCPD/' -i /etc/default/isc-dhcp-server
   sed -r "s/^INTERFACES=\".*\"/INTERFACES=\"$interface\"/" -i /etc/default/isc-dhcp-server
   
-  sed "/# --++-- $interface/,/^}$/d" -i /etc/dhcp/dhcpd.conf
+  sed "/^# --++--/,/^# ++--++/d" -i /etc/dhcp/dhcpd.conf
   cat >> /etc/dhcp/dhcpd.conf <<EOF
 # --++-- $interface
 subnet 192.168.1.0 netmask 255.255.255.0 {
   range 192.168.1.1 192.168.1.25;
   option domain-name-servers 8.8.4.4;
-  option domain-name "home";
   option routers $router_ip;
 }
+# ++--++ $interface
 EOF
-
-  service dhcpcd restart
 }
 
 function configure_network_interfaces {
@@ -76,7 +72,17 @@ function configure_bridge {
   iptables-save > /etc/iptables.ipv4.nat
 }
 
+function restart_services {
+  service dhcpcd restart
+
+  service hostapd restart
+  # hostapd /etc/hostapd/hostapd.conf
+
+  tail -f /var/log/syslog
+}
+
 configure_hostapd
-configure_dhcp
-configure_network_interfaces
 configure_bridge
+configure_network_interfaces
+configure_dhcp
+restart_services
